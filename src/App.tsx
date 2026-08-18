e507c69af9ffcb46f5bf2ebbfacd304391e4bca2
SaraCarlenis <sara.hurtado.corte@hotmail.com>
2026-08-18 20:23:09 -0300

    :tada: Adicionando Novas Funcionalidades

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/NavBar";
 import Home from "./pages/home/Home"
-
+import Cadastro from "./pages/cadastro/Cadastro";
+import Login from "./pages/login/Login";
 
 function App() {
 
   return (
     <>
-    <Home />
+    <BrowserRouter>
+    <Routes>
+    <Navbar />
+    <div className="min-h-[80vh]">
+    <Route path='/' element={<Home/>} />
+    <Route path='/cadastro' element={<Cadastro/>} />
+    <Route path='/login' element={<Login/>} />
+    </Routes>
+    </div>
+    <Footer />
+    </BrowserRouter>
     </>
   )
 }
